08d9db6ab52b1d0273bdea592a96da9de45c4dc5
Oscar Brink <oscar_brink95@msn.com>
2018-10-15 22:40:23 +0200

    clean and comment Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,26 +1,55 @@
 package controller;
 
 import model.Model;
-import org.xml.sax.SAXException;
 
+import org.xml.sax.SAXException;
 import java.io.IOException;
 
+/**
+ * Controller class taking input from the GUI in the view, and pushes it
+ * through to the model.
+ *
+ * @author  Oscar Brink
+ *          2018-10-15
+ */
 public class Controller {
 
     private Model model;
 
+    /**
+     * Constructor.
+     *
+     * @param model Model containing the data-model for the weather
+     *              application.
+     */
     public Controller(Model model) {
         this.model = model;
     }
 
-    public void setLeaseTime(String cacheTime)
+    /**
+     * Sets the lease-time, which is the time that retrieved weather-data
+     * should be cached.
+     *
+     * @param leaseTime String containing the lease-time
+     * @throws IllegalArgumentException
+     * @throws NumberFormatException
+     */
+    public void setLeaseTime(String leaseTime)
             throws IllegalArgumentException, NumberFormatException {
-        if (cacheTime == null) {
+        if (leaseTime == null) {
             return;
         }
-        this.model.setLeaseTime(Long.valueOf(cacheTime) * 60);
+        this.model.setLeaseTime(Long.valueOf(leaseTime) * 60);
     }
 
+    /**
+     * Takes a request for weather-data and forwards the request to the
+     * data-model
+     *
+     * @param placeName Name of the place that data should be retrieved for.
+     * @param date Specified date that data should be retrieved for.
+     * @param time Specified hour that data should be retrieved for.
+     */
     public void request(String placeName, String date, String time) {
         try {
             this.model.request(placeName, date, time);
